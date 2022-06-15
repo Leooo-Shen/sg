efd8f2cc8881f77279862de64384ab4e1457a55a
chengzhi <chengzhi.shen@tum.de>
2022-06-15 16:22:50 +0800

    Jun15

--- scripts/download_coco.sh
+++ scripts/download_coco.sh
@@ -15,12 +15,12 @@
 # limitations under the License.
 
 COCO_DIR=datasets/coco
-# mkdir -p $COCO_DIR
+mkdir -p $COCO_DIR
 
-# wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -O $COCO_DIR/annotations_trainval2017.zip
-# wget http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip -O $COCO_DIR/stuff_annotations_trainval2017.zip
-# wget http://images.cocodataset.org/zips/train2017.zip -O $COCO_DIR/train2017.zip
-# wget http://images.cocodataset.org/zips/val2017.zip -O $COCO_DIR/val2017.zip
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -O $COCO_DIR/annotations_trainval2017.zip
+wget http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip -O $COCO_DIR/stuff_annotations_trainval2017.zip
+wget http://images.cocodataset.org/zips/train2017.zip -O $COCO_DIR/train2017.zip
+wget http://images.cocodataset.org/zips/val2017.zip -O $COCO_DIR/val2017.zip
 
 unzip $COCO_DIR/annotations_trainval2017.zip -d $COCO_DIR
 unzip $COCO_DIR/stuff_annotations_trainval2017.zip -d $COCO_DIR
